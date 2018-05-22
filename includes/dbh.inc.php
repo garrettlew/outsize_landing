bb96f418497ae844ff3911de6c096efa68ba9068
Garrett Lew <garrett.r.lew@gmail.com>
2018-05-22 15:36:28 -0700

    backend 1

---
+++ includes/dbh.inc.php
@@ -0,0 +1,15 @@
+<!-- 
+Outsize.net
+Garrett Lew
+May 22 2018
+ -->
+<?php
+	
+	$dbServername = "localhost";
+	$dbUsername = "root";
+	$dbPassword = "";	
+	$dbName = "loginsystem";
+
+	$conn = mysql_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
