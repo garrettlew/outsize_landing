cfa3b082b4dc760e5f08fa8c849dfcce7265c4ad
Garrett Lew <garrett.r.lew@gmail.com>
2018-06-10 09:25:36 -0700

    Protect

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -7,13 +7,17 @@ May 22 2018
 <?php
     include_once 'dbh.inc.php';
 
-    $first = $_POST['name'];
-    $email = $_POST['email'];
+    $first = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
 
 
-    $sql = "INSERT INTO emails_list (first_names, emails) VALUES ('$first', '$email');";
+    $sql = "INSERT INTO emails_list (first_names, emails) VALUES (?, ?);";
 
-    mysqli_query($conn, $sql);
+    $stmt = mysqli_stmt_init($conn);
+    if (mysqli_stmt_prepare($stmt, $sql)) {
+        mysqli_stmt_bind_param($stmt, 'ss', $first, $email);
+        mysqli_stmt_execute($stmt);
+    }
 
     header("Location: ../default.html?signup=success");
 
